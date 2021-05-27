927c6f2947b52af0df3f6a624dd4bc0596e21d6d
gnana-sudha-au13 <gnanasudhau@gmail.com>
2021-05-27 23:25:11 +0530

    Database Connected First Commit

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route Files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load ENV vars
 dotenv.config({path: 'config/config.env'});
+
+// Connect to server
+connectDB();
+
+// Route Files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -19,4 +24,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+  PORT, 
+  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold)
+);
+
+// Handel Unhandled promise rejection
+process.on('unhandledRejection', (err, promise)=>{
+  console.log(`Error: ${err.message}`.red);
+  //close server & exit process
+  server.close(()=> process.exit(1));
+});
