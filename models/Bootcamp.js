4de4f86317954614ab1dddfa0df4ec3c7ffa0d99
gnana-sudha-au13 <gnanasudhau@gmail.com>
2021-06-02 19:30:17 +0530

    Middleware

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const slugify = require('slugify');
+
 
 const BootcampSchema = new mongoose.Schema(
   {
@@ -105,5 +107,11 @@ const BootcampSchema = new mongoose.Schema(
   },
 );
 
+//  Create bootcamp slug from the name
+BootcampSchema.pre('save', function(next){
+  this.slug = slugify(this.name, {lower:true});
+  next();
+
+});
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
