ea413676c194cbb300574587ea77981402eb0740
gnana-sudha-au13 <anshulco97@gmail.com>
2021-06-04 19:39:01 +0530

    Auth Protect middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,10 +17,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role
   });
 
-  // create token
-  const token = user.getSignedJwtToken();
-  res.status(200).json({success:true, token});
-  //sendTokenResponse(user, 200, res);
+  sendTokenResponse(user, 200, res);
 });
 
 // @desc      Login user
@@ -47,11 +44,9 @@ exports.login = asyncHandler(async (req, res, next) => {
   if (!isMatch) {
     return next(new ErrorResponse('Invalid credentials', 401));
   }
-  // create token
-  const token = user.getSignedJwtToken();
-  res.status(200).json({success:true, token});
 
-//   sendTokenResponse(user, 200, res);
+
+  sendTokenResponse(user, 200, res);
   });
 
 // // @desc      Log user out / clear cookie
@@ -69,17 +64,17 @@ exports.login = asyncHandler(async (req, res, next) => {
 //   });
 // });
 
-// // @desc      Get current logged in user
-// // @route     POST /api/v1/auth/me
-// // @access    Private
-// exports.getMe = asyncHandler(async (req, res, next) => {
-//   const user = await User.findById(req.user.id);
+// @desc      Get current logged in user
+// @route     POST /api/v1/auth/me
+// @access    Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
 
-//   res.status(200).json({
-//     success: true,
-//     data: user
-//   });
-// });
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
 
 // // @desc      Update user details
 // // @route     PUT /api/v1/auth/updatedetails
@@ -192,27 +187,27 @@ exports.login = asyncHandler(async (req, res, next) => {
 //   sendTokenResponse(user, 200, res);
 // });
 
-// // Get token from model, create cookie and send response
-// const sendTokenResponse = (user, statusCode, res) => {
-//   // Create token
-//   const token = user.getSignedJwtToken();
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  // Create token
+  const token = user.getSignedJwtToken();
 
-//   const options = {
-//     expires: new Date(
-//       Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-//     ),
-//     httpOnly: true
-//   };
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
 
-//   if (process.env.NODE_ENV === 'production') {
-//     options.secure = true;
-//   }
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
 
-//   res
-//     .status(statusCode)
-//     .cookie('token', token, options)
-//     .json({
-//       success: true,
-//       token
-//     });
-// };
+  res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({
+      success: true,
+      token
+    });
+};
