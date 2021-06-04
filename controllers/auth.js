496795db68e9a3d7bbd66154d0a0eb9e04cd5e9d
gnana-sudha-au13 <anshulco97@gmail.com>
2021-06-05 05:07:38 +0530

    Update user details

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,20 +49,20 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
   });
 
-// // @desc      Log user out / clear cookie
-// // @route     GET /api/v1/auth/logout
-// // @access    Private
-// exports.logout = asyncHandler(async (req, res, next) => {
-//   res.cookie('token', 'none', {
-//     expires: new Date(Date.now() + 10 * 1000),
-//     httpOnly: true
-//   });
+// @desc      Log user out / clear cookie
+// @route     GET /api/v1/auth/logout
+// @access    Private
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  });
 
-//   res.status(200).json({
-//     success: true,
-//     data: {}
-//   });
-// });
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
 
 // @desc      Get current logged in user
 // @route     POST /api/v1/auth/me
@@ -76,42 +76,42 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   });
 });
 
-// // @desc      Update user details
-// // @route     PUT /api/v1/auth/updatedetails
-// // @access    Private
-// exports.updateDetails = asyncHandler(async (req, res, next) => {
-//   const fieldsToUpdate = {
-//     name: req.body.name,
-//     email: req.body.email
-//   };
+// @desc      Update user details
+// @route     PUT /api/v1/auth/updatedetails
+// @access    Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email
+  };
 
-//   const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
-//     new: true,
-//     runValidators: true
-//   });
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true
+  });
 
-//   res.status(200).json({
-//     success: true,
-//     data: user
-//   });
-// });
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
 
-// // @desc      Update password
-// // @route     PUT /api/v1/auth/updatepassword
-// // @access    Private
-// exports.updatePassword = asyncHandler(async (req, res, next) => {
-//   const user = await User.findById(req.user.id).select('+password');
+// @desc      Update password
+// @route     PUT /api/v1/auth/updatepassword
+// @access    Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
 
-//   // Check current password
-//   if (!(await user.matchPassword(req.body.currentPassword))) {
-//     return next(new ErrorResponse('Password is incorrect', 401));
-//   }
+  // Check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse('Password is incorrect', 401));
+  }
 
-//   user.password = req.body.newPassword;
-//   await user.save();
+  user.password = req.body.newPassword;
+  await user.save();
 
-//   sendTokenResponse(user, 200, res);
-// });
+  sendTokenResponse(user, 200, res);
+});
 
 // @desc      Forgot password
 // @route     POST /api/v1/auth/forgotpassword
