77a1fe868f4ab3cf71666eaf47b85e793fec6189
gnana-sudha-au13 <anshulco97@gmail.com>
2021-06-04 14:20:10 +0530

    Single Course and Add Course

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -99,11 +99,11 @@ const BootcampSchema = new mongoose.Schema(
       type: Date,
       default: Date.now
     },
-    user: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'User'
-    }
-  },
+  
+  },{
+    toJSON: { virtuals:true},
+    toObject: { virtuals:true}
+  }
 );
 
 //  Create bootcamp slug from the name
@@ -130,6 +130,20 @@ BootcampSchema.pre('save', async function(next){
   // Do not save address in DB
   this.address = undefined;
   next();
+});
+
+// Cascade delete courses when a bootcamp is deleted 
+BootcampSchema.pre('remove', async function (next){
+  console.log(`Courses removed from bootcamp ${this._id}`);
+  await this.model('Course').deleteMany({ bootcamp: this._id});
+});
+
+// Reverse Populate with virtuals
+BootcampSchema.virtual('courses', {
+  ref: 'Course',
+  localField: '_id',
+  foreignField: 'bootcamp',
+  justOne: false
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
