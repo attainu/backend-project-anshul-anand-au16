9476de656872a598175ca523199c1dd834dcc5dd
gnana-sudha-au13 <anshulco97@gmail.com>
2021-06-05 04:54:13 +0530

    Reset Password

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,9 @@ const {
   register,
   login,
   // logout,
-  getMe
-  // forgotPassword,
-  // resetPassword,
+  getMe,
+  forgotPassword,
+  resetPassword
   // updateDetails,
   // updatePassword
 } = require('../controllers/auth'); 
@@ -20,7 +20,7 @@ router.post('/login', login);
 router.get('/me', protect, getMe);
 // router.put('/updatedetails', protect, updateDetails);
 // router.put('/updatepassword', protect, updatePassword);
-// router.post('/forgotpassword', forgotPassword);
-// router.put('/resetpassword/:resettoken', resetPassword);
+router.post('/forgotpassword', forgotPassword);
+router.put('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
