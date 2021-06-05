78b39ac9f27461f5453a050f53ac9bcb91af660b
gnana-sudha-au13 <anshulco97@gmail.com>
2021-06-05 21:37:47 +0530

    Rate Limiting, HPP & CORS

--- seeder.js
+++ seeder.js
@@ -1,19 +1,18 @@
 const fs = require('fs');
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const colors = require('colors');
+const dotenv = require('dotenv');
 
 // Load env vars
-dotenv.config({path: './config/config.env'});
+dotenv.config({ path: './config/config.env' });
 
-// load models
+// Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
 const User = require('./models/User');
 const Review = require('./models/Review');
 
-// connect to db
-
+// Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -23,51 +22,51 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Read JSON files
 const bootcamps = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/bootcamps.json`,'utf-8' )
-  );
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
 
 const courses = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/courses.json`,'utf-8' )
-  );
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 
 const users = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/users.json`,'utf-8' )
-  );
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
 
 const reviews = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/reviews.json`,'utf-8' )
-  );
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
+);
 
-// import in db
-const importData = async () =>{
+// Import into DB
+const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
     await Review.create(reviews);
-    console.log('Data Imported..'.green.inverse);
+    console.log('Data Imported...'.green.inverse);
     process.exit();
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
-}
+};
 
-// Delete data 
-const deleteData = async () =>{
+// Delete data
+const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
     await User.deleteMany();
     await Review.deleteMany();
-    console.log('Data Destroyed..'.red.inverse);
+    console.log('Data Destroyed...'.red.inverse);
     process.exit();
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
-}
+};
 
 if (process.argv[2] === '-i') {
   importData();
-} else if(process.argv[2] === '-d'){
+} else if (process.argv[2] === '-d') {
   deleteData();
 }
