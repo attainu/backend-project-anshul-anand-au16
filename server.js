fbcd7c19b846fb00aeb85832ae10f5bd15aef680
Anshul Anand <49533665+anshul-anand-au16@users.noreply.github.com>
2021-06-07 08:52:22 +0530

    Making it better

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const reviews = require('./routes/reviews');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({msg: "server is running fine"});
+  res.json({msg: "Welcome to Bootcamper!!"});
 });
 
 // Body parser
